d4058bd137c44bf3d0a5ba1f6face5e3f0076c9c
Manuel de la Fuente <git@manuelfte.com>
2020-07-11 15:29:51 -0500

    Specifying globals for ESLint; renaming some stuff.

--- getFandomFounder.js
+++ getFandomFounder.js
@@ -1,5 +1,7 @@
 
 (async () => {
+  /* eslint-env browser */
+  /* global fetch wgCityId wgContentLanguage */
   'use strict';
 
   const getJSON = async (url) => {
@@ -10,7 +12,7 @@
     });
 
     if (!response.ok) {
-      return console.log('HTTP-Error: ' + response.status);
+      return console.log('HTTP Error: ' + response.status);
     }
 
     const json = await response.json();
@@ -18,24 +20,24 @@
     return json;
   };
 
-  const wikiDetails = await getJSON(`https://community.fandom.com/api/v1/Wikis/Details?ids=${wgCityId}`);
-  const founderID = wikiDetails?.items?.[wgCityId]?.founding_user_id;
+  const wikiInfo = await getJSON(`https://community.fandom.com/api/v1/Wikis/Details?ids=${wgCityId}`);
+  const founderID = wikiInfo?.items?.[wgCityId]?.founding_user_id;
 
   if (!founderID || founderID === '0') {
     return console.log('Founder not found.');
   }
 
   const lang = wgContentLanguage === 'en' ? null : wgContentLanguage;
-  const founderDetails = await getJSON('/' + [lang, `api/v1/User/Details?ids=${founderID}`].filter(Boolean).join('/'));
-  const founderName = founderDetails.items?.[0]?.title;
+  const founderInfo = await getJSON('/' + [lang, `api/v1/User/Details?ids=${founderID}`].filter(Boolean).join('/'));
+  const founderName = founderInfo.items?.[0]?.title;
 
   if (!founderName) {
     return console.log('Founder not found.');
   }
 
-  const founderProfile = [`https://${window.location.hostname}`, lang, `wiki/User:${founderName}`].filter(Boolean).join('/');
+  const founderProfileURL = [`https://${window.location.hostname}`, lang, `wiki/User:${founderName}`].filter(Boolean).join('/');
 
   console.log('Founder found!');
   console.log('Username: ' + founderName);
-  console.log('URL: ' + founderProfile);
+  console.log('Profile: ' + founderProfileURL);
 })();
